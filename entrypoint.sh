1cd708cf52719667d1c9ad45fcd10ed363241a76
Qarani-m <emqarani@gmail.com>
2026-04-24 16:56:07 +0300

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,10 @@ set -e
 echo "Running database migrations..."
 
 migrate -path ./internal/migrations \
-  -database "$DB_URL" up
+  -database "postgres://postgres-staging-user:postgres-staging-password@localhost:5432/llm_db?sslmode=disable" up
+
+echo "Starting application..."
+
 
 echo "Starting application..."
 
