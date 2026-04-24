b5d9e252eee99af40fcd506d121f11302a268c57
Qarani-m <emqarani@gmail.com>
2026-04-24 21:05:04 +0300

    chore: update database migration host to point to postgres-staging service

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 echo "Running database migrats"
 
 migrate -path ./internal/migrations \
-  -database "postgres://postgres-staging-user:postgres-staging-password@localhost:5432/llm_db?sslmode=disable" up
+  -database "postgres://postgres-staging-user:postgres-staging-password@postgres-staging:5432/llm_db?sslmode=disable" up
 
 echo "Starting application..."
 
